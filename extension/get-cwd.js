f555073580cd172d504f819d72ea759950ab2267
ericTurf <eric.turriff@gmail.com>
2019-09-21 21:03:26 -0400

    Fixed deeply nested projects

--- extension/get-cwd.js
+++ extension/get-cwd.js
@@ -1,19 +1,30 @@
 const fs = require('fs');
 const path = require('path');
+const { window } = require('vscode');
 
 module.exports = function(rootPath, pathToFile) {
   const filePaths = pathToFile.split('/');
 
-  if (fs.readdirSync(rootPath).includes('package.json')) return rootPath;
+  // Last element is always a file so remove it
+  filePaths.pop();
 
-  return filePaths.reduce((acc, directoryName, index) => {
-    if (acc) return acc;
-    const dirFiles = fs.readdirSync(
-      path.join(rootPath, filePaths.slice(0, index + 1).join('/'))
-    );
-    if (dirFiles.includes('package.json'))
-      acc = path.join(rootPath, directoryName);
+  const rootHasPackageJSON = fs.readdirSync(rootPath).includes('package.json');
+
+  const nestedDirectory = filePaths.reduce((acc, directoryName) => {
+    const directoryIndex = filePaths.findIndex(p => p === directoryName);
+
+    const restOfFilePaths = filePaths.slice(0, directoryIndex);
+
+    const dirFiles = fs.readdirSync(path.join(rootPath, ...restOfFilePaths, directoryName));
+    if (dirFiles.includes('package.json')) {
+      acc = path.join(rootPath, ...restOfFilePaths, directoryName);
+    }
 
     return acc;
   }, '');
+
+  if (nestedDirectory) return nestedDirectory;
+  if (rootHasPackageJSON) return rootPath;
+
+  window.showErrorMessage(`None of the following directories contain a package.json. ${filePaths.join(', ')}`);
 };
--- extension/get-test-file-path.js
+++ extension/get-test-file-path.js
@@ -1,78 +1,73 @@
-const { workspace } = require('vscode');
+const { workspace, window } = require('vscode');
 const fs = require('fs');
 const path = require('path');
 
-const testFileRegex = /(\w*).((spec\b)|(test\b))\.([jt]s)/;
+const logger = require('./logger');
+
+const testFileRegex = /\.((spec)|(test))\.([jt]s)/;
 
 const testFolderRegex = /^((spec\b)|(tests\b)|(__tests__\b))/;
 
-module.exports = ({ filePath, outlookChannel, showErrorMessage }) => {
-  outlookChannel.appendLine('Enter getTestFilePath');
+const deriveTestFile = (directoryPath, fileName) => {
+  const potentialTestFiles = fs.readdirSync(directoryPath).filter(file => testFileRegex.test(file));
+
+  logger.log(`Potential test for ${fileName}: ${potentialTestFiles.join(', ')}`);
+
+  const { name: fileNameNoExt } = path.parse(fileName);
+  const specificTestFile = potentialTestFiles.find(file => file.includes(fileNameNoExt));
+
+  if (specificTestFile) {
+    logger.log(`Found resulting test file for selected file: ${specificTestFile}`);
+    return path.join(directoryPath, specificTestFile);
+  }
+
+  if (potentialTestFiles.length === 1) {
+    logger.log(`Properly derived proper test file: ${potentialTestFiles[0]}`);
+    return path.join(directoryPath, potentialTestFiles[0]);
+  } else if (potentialTestFiles.length > 1) {
+    const errorMessage = `There are too many potential test files. ${potentialTestFiles.join(
+      ', '
+    )}. Please run within the spec file`;
+
+    window.showErrorMessage(errorMessage);
+    logger.log(errorMessage);
+
+    return '';
+  }
+};
+
+module.exports = filePath => {
+  logger.log('Enter getTestFilePath');
 
   if (testFileRegex.test(filePath)) {
-    outlookChannel.appendLine(`Test file selected, simply return it ${filePath}`);
+    logger.log(`Test file selected, simply return it ${filePath}`);
     return filePath;
   }
 
-  const fileName = filePath.split('/').pop();
+  const filePathParts = filePath.split('/');
 
-  const directory = filePath
-    .split('/')
-    .filter(el => el !== fileName)
-    .join('/');
+  const fileName = filePathParts.pop();
 
-  outlookChannel.appendLine(`Looking for test files in directory: ${directory}`);
+  const {
+    uri: { fsPath },
+  } = workspace.workspaceFolders[0];
 
-  const directoryFiles = fs.readdirSync(path.join(workspace.rootPath, directory));
+  const directory = filePathParts.join('/');
+
+  logger.log(`Looking for test files in directory: ${directory}`);
+
+  const directoryFiles = fs.readdirSync(path.join(fsPath, directory));
   const hasTestDirectory = directoryFiles.some(file => testFolderRegex.test(file));
 
   if (hasTestDirectory) {
-    outlookChannel.appendLine('Test Directory (tests | __tests__ | spec) found. Looking in it for test files');
+    logger.log('Test Directory (tests | __tests__ | spec) found. Looking in it for test files');
 
     const testDirectoryName = directoryFiles.find(file => testFolderRegex.test(file));
-    const testDirectoryContent = fs.readdirSync(path.join(workspace.rootPath, directory, testDirectoryName));
-    const potentialTestFiles = testDirectoryContent.filter(file => testFileRegex.test(file));
-    const { name: fileNameNoExt } = path.parse(fileName);
-    const specificTestFile = potentialTestFiles.find(file => file.includes(fileNameNoExt));
 
-    if (specificTestFile) {
-      outlookChannel.appendLine(`Found resulting test file for selected file: ${specificTestFile}`);
-      return path.join(directory, testDirectoryName, specificTestFile);
-    }
-
-    if (potentialTestFiles.length > 1) {
-      const errorMessage = `There are too many potential test files. ${potentialTestFiles.join(
-        ', '
-      )}. Please run within the spec file`;
-
-      showErrorMessage(errorMessage);
-      outlookChannel.appendLine(errorMessage);
-    } else if (potentialTestFiles.length === 1) {
-      outlookChannel.appendLine(`Properly derived proper test file: ${potentialTestFiles[0]}`);
-      return path.join(directory, testDirectoryName, potentialTestFiles[0]);
-    }
+    return deriveTestFile(path.join(fsPath, directory, testDirectoryName), fileName);
   } else {
-    outlookChannel.appendLine('No test directory found. Looking in file directory');
+    logger.log('No test directory found. Looking in file directory');
 
-    const potentialTestFiles = directoryFiles.filter(file => testFileRegex.test(file));
-    const { name: fileNameNoExt } = path.parse(fileName);
-    const specificTestFile = potentialTestFiles.find(file => file.includes(fileNameNoExt));
-
-    if (specificTestFile) {
-      outlookChannel.appendLine(`Found resulting test file for selected file: ${specificTestFile}`);
-      return path.join(directory, specificTestFile);
-    }
-
-    if (potentialTestFiles.length === 1) {
-      outlookChannel.appendLine(`Properly derived proper test file: ${potentialTestFiles[0]}`);
-      return path.join(directory, potentialTestFiles[0]);
-    } else if (potentialTestFiles.length > 1) {
-      const errorMessage = `There are too many potential test files. ${potentialTestFiles.join(
-        ', '
-      )}. Please run within the spec file`;
-
-      showErrorMessage(errorMessage);
-      outlookChannel.appendLine(errorMessage);
-    }
+    return deriveTestFile(path.join(fsPath, directory), fileName);
   }
 };
